781a871b51ff292a130effad54baf29ff55d29b7
Andrew Pam <apam@infoxchange.org>
2019-12-03 15:11:31 +1100

    Import all symbols from sourcetraildb package into indexer package

--- indexer.pm
+++ indexer.pm
@@ -14,6 +14,9 @@ use Readonly;
 use _version qw($SOURCETRAIL_DB_VERSION);
 use sourcetraildb;
 
+# Import all symbols from sourcetraildb package
+BEGIN { $indexer::{$_} = $sourcetraildb::{$_} foreach ( keys %sourcetraildb:: ); }
+
 our @EXPORT_OK = qw(index_source_file is_sourcetraildb_version_compatible);
 our %EXPORT_TAGS = ( all => \@EXPORT_OK );
 
@@ -24,7 +27,7 @@ Readonly my %PRAGMAS => map { $_ => 1 }
 
 my ( $file_id, $main_id, $package, $package_id );
 
-sub _encode_symbol {
+sub encode_symbol {
 	my %args   = @_;
 	my %symbol = (
 		name_delimiter => '::',
@@ -34,7 +37,7 @@ sub _encode_symbol {
 		]
 	);
 	return encode_json( \%symbol );
-} ## end sub _encode_symbol
+} ## end sub encode_symbol
 
 sub index_global_variables {
 	my ($node) = @_;
@@ -43,16 +46,16 @@ sub index_global_variables {
 		foreach my $child ( $node->schildren() ) { index_global_variables($child); }
 
 		return;
-	} ## end if ( $node->class eq 'PPI::Structure::List')
+	}
 
 	return unless $node->class eq 'PPI::Token::Symbol';
 
 	my ( $sigil, $name ) = $node->content =~ qr/ ([\W]+) (.+) /x;
-	my $symbol_id = sourcetraildb::recordSymbol( _encode_symbol( prefix => $sigil, name => $name ) );
-	sourcetraildb::recordSymbolDefinitionKind( $symbol_id, $sourcetraildb::DEFINITION_EXPLICIT );
-	sourcetraildb::recordSymbolKind( $symbol_id, $sourcetraildb::SYMBOL_GLOBAL_VARIABLE );
-	sourcetraildb::recordSymbolLocation( $symbol_id, $file_id, $node->line_number, $node->column_number,
-		$node->line_number, $node->column_number + length( $node->content ) - 1 );
+	my $symbol_id = recordSymbol( encode_symbol( prefix => $sigil, name => $name ) );
+	recordSymbolDefinitionKind( $symbol_id, $DEFINITION_EXPLICIT );
+	recordSymbolKind( $symbol_id, $SYMBOL_GLOBAL_VARIABLE );
+	recordSymbolLocation( $symbol_id, $file_id, $node->line_number, $node->column_number, $node->line_number,
+		$node->column_number + length( $node->content ) - 1 );
 
 	return;
 } ## end sub index_global_variables
@@ -60,15 +63,15 @@ sub index_global_variables {
 sub index_include {
 	my ($node) = @_;
 
-	my $kind = $node->schild(0) eq 'use' ? $sourcetraildb::REFERENCE_IMPORT : $sourcetraildb::REFERENCE_INCLUDE;
+	my $kind = $node->schild(0) eq 'use' ? $REFERENCE_IMPORT : $REFERENCE_INCLUDE;
 	my $name = $node->schild(1);
 	return if $name->class ne 'PPI::Token::Word' || $PRAGMAS{ $name->content };
 
-	my $name_id = sourcetraildb::recordSymbol( _encode_symbol( name => $name->content ) );
-	sourcetraildb::recordSymbolKind( $name_id, $sourcetraildb::SYMBOL_PACKAGE );
-	sourcetraildb::recordReference( $package_id || $main_id, $name_id, $kind );
-	sourcetraildb::recordReferenceLocation( $name_id, $file_id, $name->line_number, $name->column_number,
-		$name->line_number, $name->column_number + length( $name->content ) - 1 );
+	my $name_id = recordSymbol( encode_symbol( name => $name->content ) );
+	recordSymbolKind( $name_id, $SYMBOL_PACKAGE );
+	recordReference( $package_id || $main_id, $name_id, $kind );
+	recordReferenceLocation( $name_id, $file_id, $name->line_number, $name->column_number, $name->line_number,
+		$name->column_number + length( $name->content ) - 1 );
 
 	return;
 } ## end sub index_include
@@ -84,10 +87,10 @@ sub index_local_variables {
 	return unless $node->class eq 'PPI::Token::Symbol';
 
 	my ( $sigil, $name ) = $node->content =~ qr/ ([\W]+) (.+) /x;
-	my $symbol_id = sourcetraildb::recordSymbol( _encode_symbol( prefix => $sigil, name => $name ) );
-	sourcetraildb::recordSymbolDefinitionKind( $symbol_id, $sourcetraildb::DEFINITION_EXPLICIT );
-	sourcetraildb::recordSymbolLocation( $symbol_id, $file_id, $node->line_number, $node->column_number,
-		$node->line_number, $node->column_number + length( $node->content ) - 1 );
+	my $symbol_id = recordSymbol( encode_symbol( prefix => $sigil, name => $name ) );
+	recordSymbolDefinitionKind( $symbol_id, $DEFINITION_EXPLICIT );
+	recordSymbolLocation( $symbol_id, $file_id, $node->line_number, $node->column_number, $node->line_number,
+		$node->column_number + length( $node->content ) - 1 );
 
 	return;
 } ## end sub index_local_variables
@@ -97,11 +100,11 @@ sub index_package {
 
 	my $name = $node->schild(1);
 	$package = $name->content;
-	$package_id = sourcetraildb::recordSymbol( _encode_symbol( name => $package ) );
-	sourcetraildb::recordSymbolDefinitionKind( $package_id, $sourcetraildb::DEFINITION_EXPLICIT );
-	sourcetraildb::recordSymbolKind( $package_id, $sourcetraildb::SYMBOL_PACKAGE );
-	sourcetraildb::recordSymbolLocation( $package_id, $file_id, $name->line_number, $name->column_number,
-		$name->line_number, $name->column_number + length( $name->content ) - 1 );
+	$package_id = recordSymbol( encode_symbol( name => $package ) );
+	recordSymbolDefinitionKind( $package_id, $DEFINITION_EXPLICIT );
+	recordSymbolKind( $package_id, $SYMBOL_PACKAGE );
+	recordSymbolLocation( $package_id, $file_id, $name->line_number, $name->column_number, $name->line_number,
+		$name->column_number + length( $name->content ) - 1 );
 
 	return;
 } ## end sub index_package
@@ -115,15 +118,15 @@ sub index_source_file {
 		exit(2);
 	}
 
-	$file_id = sourcetraildb::recordFile($source_file_path);
-	sourcetraildb::recordFileLanguage( $file_id, 'perl' );
+	$file_id = recordFile($source_file_path);
+	recordFileLanguage( $file_id, 'perl' );
 
 	$document->index_locations();
 
 #	PPI::Dumper->new( $document, whitespace => 0 )->print();
 
-	$main_id = sourcetraildb::recordSymbol( _encode_symbol( name => 'main' ) );
-	sourcetraildb::recordSymbolKind( $main_id, $sourcetraildb::SYMBOL_PACKAGE );
+	$main_id = recordSymbol( encode_symbol( name => 'main' ) );
+	recordSymbolKind( $main_id, $SYMBOL_PACKAGE );
 
 	index_statements($document);
 
@@ -149,10 +152,10 @@ sub index_symbol {
 	my ($node) = @_;
 
 	my ( $sigil, $name ) = $node->content =~ qr/ ([\W]+) (.+) /x;
-	my $symbol_id = sourcetraildb::recordSymbol( _encode_symbol( prefix => $sigil, name => $name ) );
-	sourcetraildb::recordReference( $package_id || $main_id, $symbol_id, $sourcetraildb::REFERENCE_USAGE );
-	sourcetraildb::recordReferenceLocation( $symbol_id, $file_id, $node->line_number, $node->column_number,
-		$node->line_number, $node->column_number + length( $node->content ) - 1 );
+	my $symbol_id = recordSymbol( encode_symbol( prefix => $sigil, name => $name ) );
+	recordReference( $package_id || $main_id, $symbol_id, $REFERENCE_USAGE );
+	recordReferenceLocation( $symbol_id, $file_id, $node->line_number, $node->column_number, $node->line_number,
+		$node->column_number + length( $node->content ) - 1 );
 
 	return;
 } ## end sub index_symbol
@@ -167,7 +170,7 @@ sub index_variables {
 } ## end sub index_variables
 
 sub is_sourcetraildb_version_compatible {
-	my $db_version = eval { sourcetraildb::getVersionString() };
+	my $db_version = eval { getVersionString() };
 	return 1 if $db_version eq $SOURCETRAIL_DB_VERSION;
 
 	$db_version = "($db_version) " if $db_version;
